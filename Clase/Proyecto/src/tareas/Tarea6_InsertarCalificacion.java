fed6b5348d1f62e774fb664b27da5ecda94499d8
Nicolaas14 <nicolas@gmail.com>
2026-08-01 12:28:40 -0500

    Agrego mi parte de la tarea

--- Clase/Proyecto/src/tareas/Tarea6_InsertarCalificacion.java
+++ Clase/Proyecto/src/tareas/Tarea6_InsertarCalificacion.java
@@ -1,5 +1,40 @@
 package tareas;
 
-public class Tarea6_InsertarCalificacion {
+import dao.CalificacionDAO;
+import modelos.Calificacion;
+import java.util.Scanner;
 
+public class Tarea6_InsertarCalificacion extends TareaBase {
+
+    public Tarea6_InsertarCalificacion() {
+        super("Insertar Calificación",
+              "Permite agregar una nueva calificación a la base de datos");
+    }
+
+    @Override
+    public void ejecutar() {
+
+        mostrarInfo();
+
+        Scanner scanner = new Scanner(System.in);
+        CalificacionDAO dao = new CalificacionDAO();
+
+        System.out.print("Ingrese ID del estudiante: ");
+        int estudianteId = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Ingrese materia: ");
+        String materia = scanner.nextLine();
+
+        System.out.print("Ingrese nota: ");
+        double nota = scanner.nextDouble();
+
+        Calificacion calificacion = new Calificacion(estudianteId, materia, nota);
+
+        if (dao.insertar(calificacion)) {
+            System.out.println("✅ Calificación insertada correctamente");
+        } else {
+            System.out.println("❌ Error al insertar calificación");
+        }
+    }
 }
